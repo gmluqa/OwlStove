fb900cd27ea273df2b787c4a77926b6f950e652b
gmluqa <gmluqa@protonmail.com>
2023-01-31 14:03:00 +0200

    chore: following hh tutorial, writing all tests

---
+++ Blockend/test/OwlStove.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("OwlStove contract", function () {
+  it("Only allows 1 mint per address", async function () {
+    expect((await 1) == 1);
+  });
+});
