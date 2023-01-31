2cc97185768cd7c0f177bfcdf0b3978100c75c7a
gmluqa <gmluqa@protonmail.com>
2023-01-31 17:53:12 +0200

    chore: base contract written for now

--- Blockend/contracts/OwlStove.sol
+++ Blockend/contracts/OwlStove.sol
@@ -1,5 +1,50 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract OwlStove {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract OwlStove is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+    uint256 maxSupply = 33;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("OwlStove", "OwO") {
+        // init with _tokenIdCounter: 1
+        _tokenIdCounter.increment();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://Qmaa6TuP2s9pSKczHF4rwWhTKUdygrrDs8RmYYqCjP3Hye/";
+    }
+
+    function safeMint() public {
+        require(
+            totalSupply() < maxSupply,
+            "The mint has concluded, all 33 Owls are out of the stove, stay tuned for upcoming drops."
+        );
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(msg.sender, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
