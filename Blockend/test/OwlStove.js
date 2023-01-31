f05b9b88301620a6bc6eaadb2259492718da7a9d
gmluqa <gmluqa@protonmail.com>
2023-01-31 14:10:15 +0200

    chore: first tests defined

--- Blockend/test/OwlStove.js
+++ Blockend/test/OwlStove.js
@@ -1,7 +1,8 @@
 const { expect } = require("chai");
 
+// run npx hardhat test to run all the tests
+
 describe("OwlStove contract", function () {
-  it("Only allows 1 mint per address", async function () {
-    expect((await 1) == 1);
-  });
+  it("Only allows 1 mint per address", async function () {});
+  it("Only allows for 33 mints", async function () {});
 });
