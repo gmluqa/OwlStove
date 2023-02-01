ecba7e10cdde2f9d93bed0516c0df39a54422bbf
gmluqa <gmluqa@protonmail.com>
2023-02-01 16:01:13 +0200

    chore: hardhat.config.js with FTM chains

--- Blockend/hardhat.config.js
+++ Blockend/hardhat.config.js
@@ -1,6 +1,21 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+
+const FTM_TESTNET_RPC_URL = "https://rpc.ankr.com/fantom_testnet";
+const FTM_MAINNET_RPC_URL = "https://rpc.ankr.com/fantom";
+const FTM_PRIVATE_KEY = process.env.FTM_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    ftm_testnet: {
+      url: FTM_TESTNET_RPC_URL,
+      accounts: [FTM_PRIVATE_KEY],
+    },
+    ftm_mainnet: {
+      url: FTM_MAINNET_RPC_URL,
+      accounts: [FTM_PRIVATE_KEY],
+    },
+  },
 };
