a4264939120857bb66de6f90e9e4cadb3c815f11
gmluqa <gmluqa@protonmail.com>
2023-02-01 15:15:07 +0200

    chore: made tests better, both pass

--- Blockend/test/OwlStove.js
+++ Blockend/test/OwlStove.js
@@ -1,4 +1,5 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
 // run npx hardhat test to run all the tests
@@ -7,41 +8,7 @@ const { expect } = require("chai");
 describe("OwlStove contract", function () {
   async function deployOwlStoveFixture() {
     const OwlStove = await ethers.getContractFactory("OwlStove");
-    const [
-      owner,
-      addr1,
-      addr2,
-      addr3,
-      addr4,
-      addr5,
-      addr6,
-      addr7,
-      addr8,
-      addr9,
-      addr10,
-      addr11,
-      addr12,
-      addr13,
-      addr14,
-      addr15,
-      addr16,
-      addr17,
-      addr18,
-      addr19,
-      addr20,
-      addr21,
-      addr22,
-      addr23,
-      addr24,
-      addr25,
-      addr26,
-      addr27,
-      addr28,
-      addr29,
-      addr30,
-      addr31,
-      addr32,
-    ] = await ethers.getSigners();
+    const [owner, addr1] = await ethers.getSigners();
 
     const OwlStoveNft = await OwlStove.deploy();
 
@@ -52,37 +19,6 @@ describe("OwlStove contract", function () {
       OwlStoveNft,
       owner,
       addr1,
-      addr2,
-      addr3,
-      addr4,
-      addr5,
-      addr6,
-      addr7,
-      addr8,
-      addr9,
-      addr10,
-      addr11,
-      addr12,
-      addr13,
-      addr14,
-      addr15,
-      addr16,
-      addr17,
-      addr18,
-      addr19,
-      addr20,
-      addr21,
-      addr22,
-      addr23,
-      addr24,
-      addr25,
-      addr26,
-      addr27,
-      addr28,
-      addr29,
-      addr30,
-      addr31,
-      addr32,
     };
   }
   it("Only allows 1 NFT to be minted per address", async function () {
@@ -92,46 +28,34 @@ describe("OwlStove contract", function () {
     await OwlStoveNft.connect(addr1).safeMint();
     // addr1 tries to mint an additional NFT
     await expect(OwlStoveNft.connect(addr1).safeMint()).to.be.revertedWith(
-      "You already own an owl OwO."
+      "You already own an owl OwO"
     );
     expect(await OwlStoveNft.balanceOf(addr1.address)).to.equal(1);
   });
   it("Only allows 33 tokens to be minted", async function () {
-    const {
-      OwlStoveNft,
-      owner,
-      addr1,
-      addr2,
-      addr3,
-      addr4,
-      addr5,
-      addr6,
-      addr7,
-      addr8,
-      addr9,
-      addr10,
-      addr11,
-      addr12,
-      addr13,
-      addr14,
-      addr15,
-      addr16,
-      addr17,
-      addr18,
-      addr19,
-      addr20,
-      addr21,
-      addr22,
-      addr23,
-      addr24,
-      addr25,
-      addr26,
-      addr27,
-      addr28,
-      addr29,
-      addr30,
-      addr31,
-      addr32,
-    } = await loadFixture(deployOwlStoveFixture);
+    const { OwlStoveNft, owner, addr1 } = await loadFixture(
+      deployOwlStoveFixture
+    );
+
+    // owner mints 1
+    await OwlStoveNft.connect(owner).safeMint();
+
+    // 32 randoms mint
+    for (let i = 0; i < 32; i++) {
+      // new wallet
+      let newWallet = ethers.Wallet.createRandom();
+      // connect provider
+      newWallet = newWallet.connect(ethers.provider);
+      await owner.sendTransaction({
+        to: newWallet.address,
+        value: ethers.utils.parseEther("1"),
+      });
+      await OwlStoveNft.connect(newWallet).safeMint();
+    }
+
+    // Person tries to mint a new owl, should fail with mint has concluded
+    await expect(OwlStoveNft.connect(addr1).safeMint()).to.be.revertedWith(
+      "The mint has concluded, all 33 Owls are out of the stove, stay tuned for upcoming drops"
+    );
   });
 });
